d0d3936725b1a7b3854ef4e4549c1b4abcb25194
Vagishvedansh <vagishvedansh@gmail.com>
2026-07-28 19:48:08 +0530

    Fix healthHandler 404 path matching

--- main.go
+++ main.go
@@ -242,6 +242,10 @@ func main() {
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"status":"running","engine":"go-kiitcode-core"}`))
